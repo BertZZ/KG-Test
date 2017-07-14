08229eeb406b756290c537ed0818a8de661f4ab7
BertZZ <bertie.wooles@hotmail.co.uk>
2017-07-14 19:06:05 +0100

    Adds more tests to deck

--- tests/deckTests.php
+++ tests/deckTests.php
@@ -33,5 +33,63 @@ class DeckTests extends TestCase
     $length = count($array);
     $this->assertEquals(52, $length);
   }
+
+  public function testThatTheFirstCardIsTheAceofHearts()
+  {
+    $class = new Deck;
+    $class->makeDeck();
+    $array = $class->getDeck();
+    $card = $array[0];
+    $value = $card->value;
+    $suit = $card->suit;
+    $this->assertEquals("Ace", $value);
+    $this->assertEquals("Heart",$suit);
+  }
+
+  public function testThatTheLastCardIsTheKingOfDiamonds()
+  {
+    $class = new Deck;
+    $class->makeDeck();
+    $array = $class->getDeck();
+    $card = $array[51];
+    $value = $card->value;
+    $suit = $card->suit;
+    $this->assertEquals("King", $value);
+    $this->assertEquals("Diamond",$suit);
+  }
+// These tests are probably not needed but I wanted peace of mind that some of the cards were where I expected them to be
+  public function testThatThe14thCardIsTheAceofClubs()
+  {
+    $class = new Deck;
+    $class->makeDeck();
+    $array = $class->getDeck();
+    $card = $array[13];
+    $value = $card->value;
+    $suit = $card->suit;
+    $this->assertEquals("Ace", $value);
+    $this->assertEquals("Club",$suit);
+  }
+  public function testThatThe51stCardIsTheQueenofDiamonds()
+  {
+    $class = new Deck;
+    $class->makeDeck();
+    $array = $class->getDeck();
+    $card = $array[50];
+    $value = $card->value;
+    $suit = $card->suit;
+    $this->assertEquals("Queen", $value);
+    $this->assertEquals("Diamond",$suit);
+  }
+  public function testThatThe27thCardIsTheAceOfSpades()
+  {
+    $class = new Deck;
+    $class->makeDeck();
+    $array = $class->getDeck();
+    $card = $array[26];
+    $value = $card->value;
+    $suit = $card->suit;
+    $this->assertEquals("Ace", $value);
+    $this->assertEquals("Spade",$suit);
+  }
 }
  ?>
