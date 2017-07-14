04178fd06f4af12edd59db303b79f84c7aff14c2
BertZZ <bertie.wooles@hotmail.co.uk>
2017-07-15 00:36:39 +0100

    Game creates players on creation

--- src/game.php
+++ src/game.php
@@ -6,16 +6,28 @@ require 'dealer.php';
 
 class Game
 {
+  const NUMBER_OF_PLAYERS = 4;
+  const PLAYER_CARDS = 7;
   public $deck;
+  public $players = array();
 
   function __construct()
   {
     $this->deck = new Deck();
+    $this->createPlayers(self::NUMBER_OF_PLAYERS);
   }
 
   public function getDeck()
   {
     return $this->deck;
+  }
+
+  public function createPlayers($numberOfPlayers)
+  {
+    for($i = 1; $i <= $numberOfPlayers; $i++){
+      $player = new Player();
+      $this->players[] = $player;
+    }
   }
 }
 
--- tests/gameTests.php
+++ tests/gameTests.php
@@ -9,5 +9,15 @@ class GameTests extends TestCase
     $deck = $game->deck;
     $this->assertInstanceof(Deck::class, $deck);
   }
+
+  public function testTheGameStartsWith4Players()
+  {
+    $game = new Game;
+    $array = $game->players;
+    $player = $array[0];
+    $length = count($array);
+    $this->assertEquals(4, $length);
+    $this->assertInstanceof(Player::class, $player);
+  }
 }
 ?>
